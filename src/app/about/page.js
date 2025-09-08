6f5709592fb3de13fbeaad3cc399473a988bebec
Fateme Amirabadian <fff.amirabadian@gmail.com>
2025-09-08 18:03:57 +0330

    resolve problems

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,10 @@
-import PublicLayout from "../layouts/public-layout";
 
 export default function AboutPage() {
     return (
-      <PublicLayout>
         <main className="p-4 min-h-screen">
         <h1 className="text-3xl font-bold mb-4">درباره ما</h1>
         <p>فروشگاه ما با هدف ارائه بهترین محصولات از برندهای معتبر دنیا راه‌اندازی شده است...</p>
       </main>
-      </PublicLayout>
     );
   }
   
---
+++ src/app/cart/layout.js
@@ -0,0 +1,5 @@
+import SpecialLayout from "../layouts/special-layout";
+
+export default function ProductsLayout({ children }) {
+  return <SpecialLayout>{children}</SpecialLayout>;
+}
